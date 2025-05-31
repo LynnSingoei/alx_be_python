2b6c6bf11fc545def15c82b7f7474e60bc654a5b
Lynn Singoei <lynnsingoei@gmail.com>
2025-05-31 22:13:11 +0300

    "Punctuations"

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,11 +1,11 @@
 weather=input("What's the weather like today? (sunny/rainy/cold): ").lower()
 if weather=="sunny":
-    print("Wear a t-shirt and sunglasses")
+    print("Wear a t-shirt and sunglasses.")
 elif weather=="rainy":
-    print("Don't forget your sweater umbrella and raincoat")
+    print("Don't forget your sweater umbrella and raincoat.")
 elif weather=="cold":
-    print("Make sure to wear a warm coat and a scarf")
+    print("Make sure to wear a warm coat and a scarf.")
 else:
-    print("Sorry i do not have recommendations for this weather")
+    print("Sorry, i do not have recommendations for this weather.")
 
 
