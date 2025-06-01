ee911bb8351ee7c98c0d29626c379bf63fbc8a56
Lynn Singoei <lynnsingoei@gmail.com>
2025-06-01 13:26:27 +0300

    "More puctuations"

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -4,13 +4,13 @@ oper=input("Choose the operation (+,-,*,/): ")
 match oper:
     case "+":
         result=num1+num2
-        print(f"The result is {result}")
+        print(f"The result is {result}.")
     case "-":
         result=num1-num2
-        print(f"The result is {result}")
+        print(f"The result is {result}.")
     case "*":
         result=num1*num2
-        print(f"The result is {result}")
+        print(f"The result is {result}.")
     case "/":
         if num2==0:
             print("Cannot divide by zero.")
