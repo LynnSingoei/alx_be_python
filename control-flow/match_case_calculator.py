a475ff75a3a102a853f70a6ce18c24d997fe9941
Lynn Singoei <lynnsingoei@gmail.com>
2025-06-01 13:22:26 +0300

    "added some space"

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,6 +1,6 @@
 num1=int(input("Enter the first number: "))
 num2=int(input("Enter the second number: "))
-oper=input("Choose the operation(+,-,*,/): ")
+oper=input("Choose the operation (+,-,*,/): ")
 match oper:
     case "+":
         result=num1+num2
