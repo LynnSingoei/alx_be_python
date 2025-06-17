4f5ce234f0b6691d8d4f31e338618547497b3b2c
Lynn Singoei <lynnsingoei@gmail.com>
2025-06-17 23:25:40 +0300

    "more spellings"

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -9,7 +9,7 @@ def convert_to_fahrenheit(celcius):
     return (celcius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
 
 temperature =float(input("Enter the temperature to convert: "))
-unit = input("Is this temperature in Celcius or Fahrenheit? (C/F): ").strip().upper()
+unit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").strip().upper()
 if unit == 'F':
     converted_temp = convert_to_celcius(temperature)
     print(f"{temperature}°F is {converted_temp}°C")
