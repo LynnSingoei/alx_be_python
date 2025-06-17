dc7346ca318d735b7a91bdfc9c06104595b7bd6b
Lynn Singoei <lynnsingoei@gmail.com>
2025-06-17 21:45:37 +0300

    "changes the namings"

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,12 +1,13 @@
-def perform_operations(num1, num2, operation):
+def perform_operation(num1, num2, operation):
     if operation == 'add':
         return num1 + num2
     elif operation == 'subtract':
         return num1 - num2
-    elif operation == 'division':
+    elif operation == 'divide':
         try:
             return num1 / num2
         except ZeroDivisionError:
             return "Error: Division by zero is not allowed."
     elif operation == 'multiply':
         return num1 * num2
+    
