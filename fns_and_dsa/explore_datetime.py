a0e6907ecbe80da7ef35884e30bcdb70b1f81b40
Lynn Singoei <lynnsingoei@gmail.com>
2025-06-17 22:57:55 +0300

    "Made the explore_datetimefile"

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,13 @@
+from datetime import datetime, timedelta
+
+def display_current_datetime():
+    current_date=datetime.now()
+    print(f"Current date and time: {current_date.strftime('%Y-%m-%d %H:%M:%S')}")
+
+days=input("Enter the number of days to add to the current date")
+def calculate_furure_date():
+    cur_date=datetime.today().date()
+    future_date=cur_date+ timedelta(days=int(days))
+    print(f"Future date is:{future_date.strftime('%Y-%m-%d')}")
+display_current_datetime()
+calculate_furure_date()
