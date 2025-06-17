228d5309baf72d01d867d56013f1d01209adb4ac
Lynn Singoei <lynnsingoei@gmail.com>
2025-06-17 21:48:59 +0300

    "USED IF ELSE"

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -4,10 +4,10 @@ def perform_operation(num1, num2, operation):
     elif operation == 'subtract':
         return num1 - num2
     elif operation == 'divide':
-        try:
-            return num1 / num2
-        except ZeroDivisionError:
+        if num2 == 0:
             return "Error: Division by zero is not allowed."
+        return num1 / num2
+       
     elif operation == 'multiply':
         return num1 * num2
     
