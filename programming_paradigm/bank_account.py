e65cd358f780b83384d545f5b349d3aa9962c1b8
Lynn Singoei <lynnsingoei@gmail.com>
2025-06-22 20:44:42 +0300

    "Created a new bank_account file in a new folder"

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,15 @@
+class BankAccount:
+    def __init__(self, balance=0.0):
+        self.balance = balance
+    def deposit(self, amount):
+        self.balance +=amount
+    def withdraw (self, amount):
+        if self.balance >= amount:
+            self.balance-=amount
+            return True
+        else:
+            return False
+    def display_balance(self):
+        print(f"Current balance: ${self.balance}")
+        
+    
