26b2736f15ac05e09ef9dc5c135e9fefc72ed0a6
Lynn Singoei <lynnsingoei@gmail.com>
2025-06-28 02:54:00 +0300

    modification

--- oop/class_static_methods_demo.py
+++ oop/class_static_methods_demo.py
@@ -4,6 +4,6 @@ class Calculator:
     def add(a, b):
         return a + b
     @classmethod
-    def multiply(cls,a,b):
+    def multiply(cls, a, b):
         print(f"Calculation Type: {cls.calculation_type}")
         return a * b
