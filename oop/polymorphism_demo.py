7c6e01117c9127c2809f799de77ce0b07d06c366
Lynn Singoei <lynnsingoei@gmail.com>
2025-06-28 02:26:00 +0300

    Added a file to calculate the area of different shapes showing polymorphism

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,19 @@
+import math
+class Shape:
+    def area(self):
+        raise NotImplementedError("Subclasses must implement this method")
+
+class Rectangle(Shape):
+    def __init__(self, width, length):
+        self.width = width
+        self.length = length
+
+    def area(self):
+        return self.width * self.length
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return math.pi * self.radius * self.radius
